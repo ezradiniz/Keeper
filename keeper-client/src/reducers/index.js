c0973d68c3b5d2e6caf9384941b1694a8ddd6a76
ezra <linux.esdras@gmail.com>
2018-01-12 18:21:21 -0200

    reset state of redux store when user loggout

--- keeper-client/src/reducers/index.js
+++ keeper-client/src/reducers/index.js
@@ -2,6 +2,7 @@ import { combineReducers } from 'redux';
 import { createStore, applyMiddleware } from 'redux';
 import { createLogger } from 'redux-logger'
 import { composeWithDevTools } from 'redux-devtools-extension';
+import { USER_LOGGED_OUT } from '../constantes/types';
 import thunk from 'redux-thunk';
 
 import user from './user';
@@ -20,7 +21,15 @@ const reducers = combineReducers({
   log
 });
 
+const rootReducer = (state, action) => {
+  if (action.type === USER_LOGGED_OUT) {
+    state = undefined;
+  }
+
+  return reducers(state, action);
+};
+
 export default createStore(
-  reducers,
+  rootReducer,
   composeWithDevTools(applyMiddleware(...middleware))
 );
