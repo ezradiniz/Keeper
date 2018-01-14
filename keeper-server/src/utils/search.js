3ae0aace4e073661be9e6417f02e93cd1052ea66
ezra <linux.esdras@gmail.com>
2018-01-14 10:36:05 -0200

    Update fuzzy search and fix log render map

--- keeper-server/src/utils/search.js
+++ keeper-server/src/utils/search.js
@@ -3,6 +3,7 @@ import Fuse from 'fuse.js';
 var options = {
   shouldSort: true,
   keys: [
+    '_id',
     'subject',
     'body.blocks.text'
   ]
@@ -12,7 +13,7 @@ export default function(query, notes) {
   /*
    * TODO: Refactoring later
    */
-  const notesList = notes.map(n => { return { ...n, body: JSON.parse(n.body) } });
+  const notesList = notes.map(n => { return { ...n, _id: n._id.toString(), body: JSON.parse(n.body) } });
   const fuse = new Fuse(notesList, options);
   return fuse.search(query).map(n => { return { ...n, body: JSON.stringify(n.body) } });
 };
